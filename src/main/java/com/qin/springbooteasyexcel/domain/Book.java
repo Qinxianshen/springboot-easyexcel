12e48aefd22f8829410b10ea4d974387cd27ece8
Qin <mm739023340@qq.com>
2019-12-02 12:01:48 +0800

    修改Book类添加相关注解

--- src/main/java/com/qin/springbooteasyexcel/domain/Book.java
+++ src/main/java/com/qin/springbooteasyexcel/domain/Book.java
@@ -1,43 +1,64 @@
 package com.qin.springbooteasyexcel.domain;
 
+import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.metadata.BaseRowModel;
+import javafx.scene.chart.ValueAxis;
+
 /* author:Qinzijing
 *  date: 2019/12/2
 *  description:书籍实体类
 */
-public class Book {
+public class Book extends BaseRowModel {
     //书籍id
+    @ExcelProperty(value = "书籍id",index = 0)
     private Integer bookId;
     //书籍编号
+    @ExcelProperty(value = "书籍编号",index = 1)
     private Integer bookNumber;
     //书籍名称
+    @ExcelProperty(value = "书籍名称",index = 2)
     private String bookName;
     //书籍作者
+    @ExcelProperty(value = "书籍作者",index = 3)
     private String bookAuthor;
     //书籍出版社
+    @ExcelProperty(value = "书籍出版社",index = 4)
     private String bookPress;
     //书籍封皮大图
+    @ExcelProperty(value = "书籍封皮大图",index = 5)
     private String bookImageBig;
     //书籍封皮缩略图
+    @ExcelProperty(value = "书籍封皮缩略图",index = 6)
     private String bookImageSmall;
     //书籍一级分类
+    @ExcelProperty(value = "书籍一级分类",index = 7)
     private String bookClassifyOne;
     //书籍二级分类
+    @ExcelProperty(value = "书籍二级分类",index = 8)
     private String bookClassifyTwo;
     //书籍简介
+    @ExcelProperty(value = "书籍简介",index = 9)
     private String bookDesc;
     //书籍所在地址
+    @ExcelProperty(value = "书籍所在地址",index = 10)
     private String bookAddress;
     //书籍评分
+    @ExcelProperty(value = "书籍评分",index = 11)
     private Double bookGrade;
     //书籍数量
+    @ExcelProperty(value = "书籍数量",index = 12)
     private int bookSum;
     //书籍剩余
+    @ExcelProperty(value = "书籍剩余",index = 13)
     private int bookResidue;
     //书籍借阅次数
+    @ExcelProperty(value = "书籍借阅次数",index = 14)
     private int bookBorrowNum;
     //用户对书籍期望值
+    @ExcelProperty(value = "用户对书籍期望值",index = 15)
     private int bookHopeByUser;
     //书籍的状态
+    @ExcelProperty(value = "书籍的状态",index = 16)
     private int bookState;
 
     public int getBookState() {
--- src/main/java/com/qin/springbooteasyexcel/service/Impl/ExportAndInportServiceImpl.java
+++ src/main/java/com/qin/springbooteasyexcel/service/Impl/ExportAndInportServiceImpl.java
@@ -4,10 +4,12 @@ package com.qin.springbooteasyexcel.service.Impl;
 import com.qin.springbooteasyexcel.dao.BookMapper;
 import com.qin.springbooteasyexcel.domain.Book;
 import com.qin.springbooteasyexcel.service.ExportAndInportService;
+import com.qin.springbooteasyexcel.util.excel.ExcelUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 /* author:Qinzijing
@@ -19,6 +21,15 @@ public class ExportAndInportServiceImpl implements ExportAndInportService {
 
     @Autowired
     private BookMapper bookMapper;
+
+    public List<Book> exportAllBook(HttpServletResponse response) {
+        List<Book> bookList = bookMapper.findAllBook();
+        String fileName = "一个 Excel 文件";
+        String sheetName = "第一个 sheet";
+
+        ExcelUtil.writeExcel(response, bookList, fileName, sheetName, new Book());
+        return null;
+    }
 
     @Override
     public List<Book> exportAllBook() {
